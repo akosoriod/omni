a66c6956329da4059391dce5699a5d3bac7097d2
aosorio@possibilitycompany.com-at-743433884678 <akosoriod@unal.edu.co>
2022-03-15 13:05:19 -0500

    initial endpoints

--- src/helpers/utilsHelper.ts
+++ src/helpers/utilsHelper.ts
@@ -1,7 +1,7 @@
 
 
 
-export const getvalue = async (arr:Array[], key:string) : Promise<any> => {
+export const getvalue = async (arr:any, key:string) : Promise<any> => {
 
     return (arr: any[],key: string) => (arr.find((x: {}) => Object.keys(x)[0] === key) || {})[key]
      
